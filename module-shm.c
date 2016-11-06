f7a930d8ad5503d7fe8d9d7e2b07386e49ce91b2
Edgars <edgars.irmejs@gmail.com>
2016-11-06 18:31:29 +0200

    GCC optimized away shared memory accesses. Fixed that.

--- module-shm.c
+++ module-shm.c
@@ -29,13 +29,13 @@
 
 #define SHARED_MEMORY_BUF_SIZE (1024-8-8-8)
 
-typedef struct sharedMemoryBuffer {
+typedef volatile struct sharedMemoryBuffer {
     char buf[SHARED_MEMORY_BUF_SIZE];
     size_t read_idx;
     size_t write_idx;
 } sharedMemoryBuffer;
 
-typedef struct sharedMemory {
+typedef volatile struct sharedMemory {
     sharedMemoryBuffer to_server;
     sharedMemoryBuffer to_client;
 } sharedMemory;
@@ -53,6 +53,12 @@ static list *threads;
 
 /*TODO: sharedMemory buffer split out and symlinked between projects */
 
+/*inline void
+clflush(volatile void *p)
+{
+    asm volatile ("clflush (%0)" :: "r"(p));
+}*/
+
 static size_t bufFreeSpace(sharedMemoryBuffer *target) {
     size_t read_idx = target->read_idx;
     ssize_t free = (ssize_t)read_idx - target->write_idx - 1;
@@ -63,24 +69,30 @@ static size_t bufFreeSpace(sharedMemoryBuffer *target) {
 }
 
 static void bufWrite(sharedMemoryBuffer *target, char *buf, size_t btw) {
-    X("bufWrite(read_ptr=%d write_ptr=%d btw=%d\n", target->read_idx, target->write_idx, btw);
+    X("%lld bufWrite(read_ptr=%d write_ptr=%d btw=%d\n", ustime(), target->read_idx, target->write_idx, btw);
     size_t write_idx = target->write_idx;
     __sync_synchronize();
+    char* target_buf = (char*)target->buf; /* casting away volatile because buf can't change in the range being used. */
     if (write_idx >= target->read_idx) {
         size_t bytes_to_eob = sizeof(target->buf) - write_idx;
         if (bytes_to_eob <= btw) {
             /* Write needs to rotate target->write_idx to the buffer beginning */
-            memcpy(target->buf + write_idx, buf, bytes_to_eob);
+            memcpy(target_buf + write_idx, buf, bytes_to_eob);
             write_idx = 0;
             btw -= bytes_to_eob;
             buf += bytes_to_eob;
         }
     }
-    memcpy(target->buf + write_idx, buf, btw);
+    memcpy(target_buf + write_idx, buf, btw);
     write_idx += btw;
     __sync_synchronize();
     target->write_idx = write_idx;
-    X("bufWrite fin (read_ptr=%d write_ptr=%d btw=%d\n", target->read_idx, target->write_idx, btw);
+    /* Need to push out of L1 cache, for other CPUs to see the update now,
+     * but the CPU seems to do a good enough job of that. There is no way to
+     * push out to L2, only purge of all cache hierarchy, which is slow. */
+//    asm volatile ("mfence" ::: "memory");
+//    clflush(&target->write_idx);
+    X("%lld bufWrite fin (read_ptr=%d write_ptr=%d btw=%d\n", ustime(), target->read_idx, target->write_idx, btw);
 }
 
 /* TODO: Less duplication, maybe? */
@@ -95,32 +107,28 @@ static size_t bufUsedSpace(sharedMemoryBuffer *source) {
 
 /* TODO: Less duplication, maybe? */
 static void bufRead(sharedMemoryBuffer *source, char *buf, size_t btr) {
-    X("bufRead(read_ptr=%d write_ptr=%d btr=%d\n", source->read_idx, source->write_idx, btr);
+    X("%lld bufRead(read_ptr=%d write_ptr=%d btr=%d\n", ustime(), source->read_idx, source->write_idx, btr);
     size_t read_idx = source->read_idx;
     __sync_synchronize();
+    char* source_buf = (char*)source->buf; /* casting away volatile because buf can't change in the range being used. */
     if (read_idx > source->write_idx) {
         size_t bytes_to_eob = sizeof(source->buf) - read_idx;
         if (bytes_to_eob <= btr) {
             /* Read needs to rotate source->read_idx to the buffer beginning */
-            memcpy(buf, source->buf + read_idx, bytes_to_eob);
+            memcpy(buf, source_buf + read_idx, bytes_to_eob);
             read_idx = 0;
             btr -= bytes_to_eob;
             buf += bytes_to_eob;
         }
     }
-//    while (btr > 0) {
-//        char tmp = *(source->buf + read_idx);
-//        *buf = tmp;
-//        btr--;
-//        read_idx++;
-//        buf++;
-//    }
-    /* TODO: Something is wrong. memcpy failed on me before, for no reason, while a loop didn't, and then... memcpy didn't too. */
-    memcpy(buf, source->buf + read_idx, btr);
+    memcpy(buf, source_buf + read_idx, btr);
     read_idx += btr;
     __sync_synchronize();
     source->read_idx = read_idx;
-    X("bufRead fin (read_ptr=%d write_ptr=%d btr=%d\n", source->read_idx, source->write_idx, btr);
+    /* Need to push out of L1 cache, for other CPUs to see the update now. */
+//    asm volatile ("mfence" ::: "memory");
+//    clflush(&source->read_idx);
+    X("%lld bufRead fin (read_ptr=%d write_ptr=%d btr=%d\n", ustime(), source->read_idx, source->write_idx, btr);
 }
 
 static void ProcessPendingInput();
@@ -301,7 +309,7 @@ static void WriteOutput(threadCtx *thread_ctx)
 
 static void ProcessPendingInput()
 {
-//    X("blah %lld \n", ustime());
+    X("blah %lld \n", ustime());
     listNode* it = listFirst(threads);
     while (it != NULL) {
         threadCtx *thread_ctx = listNodeValue(it);
@@ -376,11 +384,11 @@ static int Command_Open(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
     /* Set up a call processing mechanism. To avoid data races with the main thread,
      * we get the main thread to do the processing at a good time.
      */
-    if (!old_beforeSleep_set) { /* Still NULL at RedisModule_OnLoad */
-        old_beforeSleep = server.el->beforesleep;
-        aeSetBeforeSleepProc(server.el, BeforeSleep_PlusShm);
-        old_beforeSleep_set = 1;
-    }
+//    if (!old_beforeSleep_set) { /* Still NULL at RedisModule_OnLoad */
+//        old_beforeSleep = server.el->beforesleep;
+//        aeSetBeforeSleepProc(server.el, BeforeSleep_PlusShm);
+//        old_beforeSleep_set = 1;
+//    }
     
     threadCtx *thread_ctx = RedisModule_Alloc(sizeof(threadCtx));
     thread_ctx->module_ctx = ctx;
@@ -389,7 +397,7 @@ static int Command_Open(RedisModuleCtx *ctx, RedisModuleString **argv, int argc)
     
     listAddNodeHead(threads, thread_ctx); /*TODO: err*/
     
-    X("%lld creating thread \n", ustime());
+    printf("%lld creating thread \n", ustime());
     int err = pthread_create(&thread_ctx->thread, NULL, RunThread, thread_ctx);
     if (err != 0) {
         RedisModule_Free(thread_ctx);
