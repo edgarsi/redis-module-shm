778d6cfeba1b8fbd923f5c4a3290d9837dfd7893
Edgars <edgars.irmejs@gmail.com>
2016-11-08 18:17:37 +0200

    (split) Rewrote the API, got rid of the duplication in FreeSpace and UsedSpace.

--- charfifo.c
+++ charfifo.c
@@ -15,20 +15,43 @@
 //#define X printf
 
 
+#define EXTRACT_HEADER(p) \
+    charfifo_header_t *header = (charfifo_header_t*)charfifo;
 
-#define SHARED_MEMORY_BUF_SIZE (1024-8-8-8)
+#define EXTRACT_FIFO_BUF(p) \
+    volatile char* fifo_buf = (char*)p + sizeof(charfifo_header_t);
 
-typedef volatile struct sharedMemoryBuffer {
-    char buf[SHARED_MEMORY_BUF_SIZE];
-    size_t read_idx;
-    size_t write_idx;
-} sharedMemoryBuffer;
+void CharFifo_Init(volatile void *charfifo, size_t size)
+{
+    EXTRACT_HEADER(charfifo);
+    header->read_idx = 0;
+    header->write_idx = 0;
+    header->size = size;
+}
 
-typedef volatile struct sharedMemory {
-    sharedMemoryBuffer to_server;
-    sharedMemoryBuffer to_client;
-} sharedMemory;
+// Given a circular buffer of size 'size', how many bytes can 'start_idx' move
+// forward until 'stop_padding' bytes to reaching 'end_idx' are left.
+inline static size_t WraparoundDiff(size_t start_idx, size_t end_idx, size_t size, size_t stop_padding)
+{
+    size_t end_idx_cpy = end_idx;
+    ssize_t diff = (ssize_t) end_idx_cpy - start_idx - stop_padding;
+    if (end_idx_cpy < start_idx + stop_padding) {
+        diff = diff + size;
+    }
+    return diff;
+}
 
+size_t CharFifo_FreeSpace(volatile void *charfifo)
+{
+    EXTRACT_HEADER(charfifo);
+    return WraparoundDiff(header->write_idx, header->read_idx, header->size, 1);
+}
+
+size_t CharFifo_UsedSpace(volatile void *charfifo)
+{
+    EXTRACT_HEADER(charfifo);
+    return WraparoundDiff(header->read_idx, header->write_idx, header->size, 0);
+}
 
 /*inline void
 clflush(volatile void *p)
@@ -36,22 +59,15 @@ clflush(volatile void *p)
     asm volatile ("clflush (%0)" :: "r"(p));
 }*/
 
-static size_t bufFreeSpace(sharedMemoryBuffer *target) {
-    size_t read_idx = target->read_idx;
-    ssize_t free = (ssize_t)read_idx - target->write_idx - 1;
-    if (read_idx <= target->write_idx) {
-        free = free + sizeof(target->buf);
-    }
-    return free;
-}
-
-static void bufWrite(sharedMemoryBuffer *target, char *buf, size_t btw) {
-    X("%lld bufWrite(read_ptr=%d write_ptr=%d btw=%d\n", ustime(), target->read_idx, target->write_idx, btw);
-    size_t write_idx = target->write_idx;
+void CharFifo_Write(volatile void *charfifo, char *buf, size_t btw) {
+    EXTRACT_HEADER(charfifo);
+    EXTRACT_FIFO_BUF(charfifo);
+    X("%lld bufWrite(read_ptr=%d write_ptr=%d btw=%d\n", ustime(), header->read_idx, header->write_idx, btw);
+    size_t write_idx = header->write_idx;
     __sync_synchronize();
-    char* target_buf = (char*)target->buf; /* casting away volatile because buf can't change in the range being used. */
-    if (write_idx >= target->read_idx) {
-        size_t bytes_to_eob = sizeof(target->buf) - write_idx;
+    char* target_buf = (char*)fifo_buf; /* casting away volatile because buf can't change in the range being used. */
+    if (write_idx >= header->read_idx) {
+        size_t bytes_to_eob = header->size - write_idx;
         if (bytes_to_eob <= btw) {
             /* Write needs to rotate target->write_idx to the buffer beginning */
             memcpy(target_buf + write_idx, buf, bytes_to_eob);
@@ -63,33 +79,25 @@ static void bufWrite(sharedMemoryBuffer *target, char *buf, size_t btw) {
     memcpy(target_buf + write_idx, buf, btw);
     write_idx += btw;
     __sync_synchronize();
-    target->write_idx = write_idx;
+    header->write_idx = write_idx;
     /* Need to push out of L1 cache, for other CPUs to see the update now,
      * but the CPU seems to do a good enough job of that. There is no way to
      * push out to L2, only purge of all cache hierarchy, which is slow. */
 //    asm volatile ("mfence" ::: "memory");
 //    clflush(&target->write_idx);
-    X("%lld bufWrite fin (read_ptr=%d write_ptr=%d btw=%d\n", ustime(), target->read_idx, target->write_idx, btw);
+    X("%lld bufWrite fin (read_ptr=%d write_ptr=%d btw=%d\n", ustime(), header->read_idx, header->write_idx, btw);
 }
 
 /* TODO: Less duplication, maybe? */
-static size_t bufUsedSpace(sharedMemoryBuffer *source) {
-    size_t write_idx = source->write_idx;
-    ssize_t used = write_idx - source->read_idx;
-    if (write_idx < source->read_idx) {
-        used = used + sizeof(source->buf);
-    }
-    return used;
-}
-
-/* TODO: Less duplication, maybe? */
-static void bufRead(sharedMemoryBuffer *source, char *buf, size_t btr) {
-    X("%lld bufRead(read_ptr=%d write_ptr=%d btr=%d\n", ustime(), source->read_idx, source->write_idx, btr);
-    size_t read_idx = source->read_idx;
+void CharFifo_Read(volatile void *charfifo, char *buf, size_t btr) {
+    EXTRACT_HEADER(charfifo);
+    EXTRACT_FIFO_BUF(charfifo);
+    X("%lld bufRead(read_ptr=%d write_ptr=%d btr=%d\n", ustime(), header->read_idx, header->write_idx, btr);
+    size_t read_idx = header->read_idx;
     __sync_synchronize();
-    char* source_buf = (char*)source->buf; /* casting away volatile because buf can't change in the range being used. */
-    if (read_idx > source->write_idx) {
-        size_t bytes_to_eob = sizeof(source->buf) - read_idx;
+    char* source_buf = (char*)fifo_buf; /* casting away volatile because buf can't change in the range being used. */
+    if (read_idx > header->write_idx) {
+        size_t bytes_to_eob = header->size - read_idx;
         if (bytes_to_eob <= btr) {
             /* Read needs to rotate source->read_idx to the buffer beginning */
             memcpy(buf, source_buf + read_idx, bytes_to_eob);
@@ -101,10 +109,10 @@ static void bufRead(sharedMemoryBuffer *source, char *buf, size_t btr) {
     memcpy(buf, source_buf + read_idx, btr);
     read_idx += btr;
     __sync_synchronize();
-    source->read_idx = read_idx;
+    header->read_idx = read_idx;
     /* Need to push out of L1 cache, for other CPUs to see the update now. */
 //    asm volatile ("mfence" ::: "memory");
 //    clflush(&source->read_idx);
-    X("%lld bufRead fin (read_ptr=%d write_ptr=%d btr=%d\n", ustime(), source->read_idx, source->write_idx, btr);
+    X("%lld bufRead fin (read_ptr=%d write_ptr=%d btr=%d\n", ustime(), header->read_idx, header->write_idx, btr);
 }
 
---
+++ charfifo.h
@@ -0,0 +1,36 @@
+/*
+ * charfifo.h
+ *
+ *  Created on: Nov 8, 2016
+ *      Author: Edgars
+ */
+
+#ifndef LOCKLESS_CHAR_FIFO_CHARFIFO_H_
+#define LOCKLESS_CHAR_FIFO_CHARFIFO_H_
+
+#include <stdlib.h>
+
+typedef struct {
+    size_t read_idx;
+    size_t write_idx;
+    size_t size;
+} charfifo_header_t;
+
+// Outputs the type of a struct having 'size' many chars in buffer.
+// Example: typedef CHARFIFO(2000) t_my_buffer;
+#define CHARFIFO(size)                                                         \
+    volatile struct { /* volatile needed iff shared memory used */             \
+        charfifo_header_t header;                                       \
+        char buf[size + 1];                                                    \
+    } __attribute__((aligned(8))) /* aligned for atomic instructions */
+
+void CharFifo_Init(volatile void *charfifo, size_t size);
+
+size_t CharFifo_FreeSpace(volatile void *charfifo);
+void CharFifo_Write(volatile void *charfifo, char *buf, size_t btw); // does not check free space!
+
+size_t CharFifo_UsedSpace(volatile void *charfifo);
+void CharFifo_Read(volatile void *charfifo, char *buf, size_t btr); // does not check used space!
+
+
+#endif /* LOCKLESS_CHAR_FIFO_CHARFIFO_H_ */
